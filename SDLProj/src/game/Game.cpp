cb38bdd83c3f6f13b4b0d770f45d00da8a25bcbb
Akshayaap <akshaykumarparmar51@gmail.com>
2022-05-21 01:33:37 +0530

    Initial Commit

---
+++ SDLProj/src/game/Game.cpp
@@ -0,0 +1,33 @@
+#include "Game.h"
+
+Game::Game(const char* title, int top, int left, int width, int height) {
+
+}
+Game::~Game() {
+
+}
+
+
+void Game::Init(const char* title, int top, int left, int width, int height) {
+	if(SDL_Init())
+}
+
+void Game::Update() {
+
+}
+
+void Game::Render() {
+
+}
+
+void Game::Clean() {
+
+}
+
+void Game::HandleEvents() {
+
+}
+bool Game::IsRunning() {
+	return this->isRunning;
+}
+
---
+++ SDLProj/src/game/Game.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <SDL.h>
+class Game
+{
+private:
+	bool isRunning;
+	SDL_Window *window;
+	SDL_Renderer* renderer;
+
+
+public:
+	Game(const char* title, int top, int left, int width, int height);
+	~Game();
+	void Init(const char* title,int top,int left, int width,int height);
+	void Update();
+	void Render();
+	void Clean();
+	void HandleEvents();
+	bool IsRunning();
+
+};
+
